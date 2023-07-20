210723ac7be547ff734752bdd65f19128cf502b6
BlueishInColour <bluishcode@gmail.com>
2023-07-20 07:56:59 +0000

    0

--- lib/Models/explore_screen_models.dart
+++ lib/Models/explore_screen_models.dart
@@ -48,19 +48,17 @@ import 'package:mysql_client/mysql_client.dart';
 ///a user or creator will need thier own table of
 /// followers it own data groubes in all_users table
 /// following it own data groubes in all_users table
-/// posts 
+/// posts
 
- final conn =MySQLConnection.createConnection(
-    host: "127.0.0.1",
-    port: 3306,
-    userName: "root",
-    password: "oluwa", // optional
- );
-  
+final conn = MySQLConnection.createConnection(
+  host: "127.0.0.1",
+  port: 3306,
+  userName: "root",
+  password: "oluwa", // optional
+);
 
 Future createDatabaseAndTable() async {
   print("Connecting to mysql server...");
-  
 
   // create connection
   final conn = await MySQLConnection.createConnection(
@@ -70,16 +68,15 @@ Future createDatabaseAndTable() async {
     password: "oluwa", // optional
   );
 
- await conn.connect();
+  await conn.connect();
 
 //after connected
   print("Connected");
 
+  var res1 = await conn.execute('CREATE DATABASE baseDB ');
+  print('res1 : $res1');
 
-var res1 = await conn.execute('CREATE DATABASE baseDB ');
-print('res1 : $res1');
-
-var res2 = await conn.execute("""CREATE TABLE baseDB.all_users(
+  var res2 = await conn.execute("""CREATE TABLE baseDB.all_users(
  index_number IDENTITY(1,1) PRIMARY KEY,
   full_name VARCHAR(225),
   user_name VARCHAR(225) PRIMARY KEY,
@@ -90,10 +87,10 @@ var res2 = await conn.execute("""CREATE TABLE baseDB.all_users(
   sex VARCHAR(1),
   type_of_user VARCHAR(225),
 
-)""") ;
-print('res2 : $res2');
+)""");
+  print('res2 : $res2');
 
-var res3 = await conn.execute("""
+  var res3 = await conn.execute("""
 CREATE TABLE baseDb.allcreators(
   creator_id IDENTITY(1,1) PRIMARY KEY,
   brand_name = VARCHAR(225),
@@ -101,22 +98,19 @@ CREATE TABLE baseDb.allcreators(
 
 )
 """);
-print('res3 : $res3');
+  print('res3 : $res3');
 }
 
-
-Future  createUser({
-  String full_name ='',
-  String user_name='',
-  String user_profile_pix = '',
- String email= '',
- String phone_number='',
- String date_of_birth = '',
- String sex = '',
- String type_of_user = ''
-  })async{
-    print("Connecting to mysql server...");
-  
+Future createUser(
+    {String full_name = '',
+    String user_name = '',
+    String user_profile_pix = '',
+    String email = '',
+    String phone_number = '',
+    String date_of_birth = '',
+    String sex = '',
+    String type_of_user = ''}) async {
+  print("Connecting to mysql server...");
 
   // create connection
   final conn = await MySQLConnection.createConnection(
@@ -126,50 +120,56 @@ Future  createUser({
     password: "oluwa", // optional
   );
 
- await conn.connect();
+  await conn.connect();
 
 //after connected
   print("Connected");
 
   //insert user into list of all users
-var res1 = await conn.execute("""
+  var res1 = await conn.execute(
+    """
 INSERT INTO 
 baseDB.all_users(full_name,user_name,user_profile_pix,email,phone_number,date_of_birth,sex,type_of_user)
 VALUES(:full_name,:user_name,:user_profile_pix,:email,:phone_number,:date_of_birth,:sex,:type_of_user)
-""", {'full_name':full_name, 'user_name':user_name,'email':email, 'phone_number':phone_number,'date_of_birth':date_of_birth,'sex':sex,'type_of_user':type_of_user ,},);
-print(res1);
+""",
+    {
+      'full_name': full_name,
+      'user_name': user_name,
+      'email': email,
+      'phone_number': phone_number,
+      'date_of_birth': date_of_birth,
+      'sex': sex,
+      'type_of_user': type_of_user,
+    },
+  );
+  print(res1);
 
 //create user database
-var res2 = await conn.execute("""CREATE DATABASE $user_name """);
-print(res2);
-
+  var res2 = await conn.execute("""CREATE DATABASE $user_name """);
+  print(res2);
 
 //create followers data table
-var res3 = await conn.execute("""CREATE TABLE $user_name.followers(
+  var res3 = await conn.execute("""CREATE TABLE $user_name.followers(
 index_number IDENTITY(1,1) ,
 user_name VARCHAR(225),
 full_name VARCHAR(225),
 followed_from DATETIME,
 )""");
 
-print(res3);
-
+  print(res3);
 
 //create following data table
-var res4 = await conn.execute(""" CREATE TABLE $user_name.following(
+  var res4 = await conn.execute(""" CREATE TABLE $user_name.following(
   index_number IDENTITY(1,1) ,
 user_name VARCHAR(225),
 full_name VARCHAR(225),
 followed_from DATETIME,
 )""");
 
-print (res4);
-
+  print(res4);
 
 //close connection
-conn.close();
+  conn.close();
+}
 
-
-  }
-  
-
+////
