366e9fdaa88e5ea9379eba822754179665c8cf76
BlueishInColour <bluishcode@gmail.com>
2023-08-19 13:18:43 +0000

    save

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import "package:flutter/material.dart";
 import 'screens/splash_screen.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+//import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   //for dontenv variables of auth0
-  await dotenv.load();
+
   //
   runApp(const Swagr());
 }
